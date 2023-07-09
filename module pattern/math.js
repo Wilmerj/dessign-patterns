62bf7ee2df261235ff880eca9c7842329533128a
Wilmer Garzon <wilmerj1996@gmail.com>
2023-07-09 12:12:11 -0500

    doc: module pattern

---
+++ module pattern/math.js
@@ -0,0 +1,22 @@
+export const secret = 123;
+
+export function sum(x, y) {
+  return x + y;
+}
+
+export function multiply(x, y) {
+  return x * y;
+}
+
+export function subtract(x, y) {
+  return x - y;
+}
+
+export function divide(x, y) {
+  return x / y;
+}
+
+console.log("Sum", sum(1, 2));
+console.log("Subtract", subtract(1, 2));
+console.log("Divide", divide(1, 2));
+console.log("Multiply", multiply(1, 2));
